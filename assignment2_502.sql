61e8d168b61c2e988668699adc00b4c7cc4c8af8
Khoi Nguyen <khoi.nguyen@futago.com.au>
2021-05-14 00:28:16 +1000

    new sql

--- assignment2_502.sql
+++ assignment2_502.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 13, 2021 at 08:41 PM
+-- Generation Time: May 14, 2021 at 12:27 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -51,8 +51,11 @@ CREATE TABLE `accommodations` (
 --
 
 INSERT INTO `accommodations` (`id`, `type`, `description`, `max_guests`, `total_rooms`, `total_bathrooms`, `address`, `published_at`, `price_per_night`, `created_at`, `updated_at`, `house_name`, `city`, `has_garage`, `has_internet`, `allow_pet`, `allow_smoke`, `owner_id`, `image_url`, `available_from`, `available_to`) VALUES
-(1, 'House', NULL, 3, 2, 1, '1/5 West Hobart', '2021-05-12', '150', '2021-05-11', NULL, 'Loft Studio in the Central Area', 'Hobart', 0, 1, 1, 0, 3, 'https://a2.muscache.com/im/pictures/6152848/b04eddeb_original.jpg?aki_policy=x_medium', '2021-05-13', '2021-05-23'),
-(2, 'House', 'Good', 2, 2, 1, '2 Collins St', '2021-05-12', '200', '2021-05-11', '', 'Sky View', 'Melbourne', 0, 1, 0, 0, 3, 'https://a2.muscache.com/im/pictures/6152848/b04eddeb_original.jpg?aki_policy=x_medium', '2021-05-13', '2021-05-15');
+(1, 'House', 'Loft Studio in the Central Area', 3, 2, 1, '1/5 West Hobart', '2021-05-12', '150', '2021-05-11', NULL, 'Loft Studio in the Central Area', 'Hobart', 0, 1, 1, 0, 3, 'https://a2.muscache.com/im/pictures/6152848/b04eddeb_original.jpg?aki_policy=x_medium', '2021-05-13', '2021-05-23'),
+(2, 'Resort', 'Sky View', 40, 20, 1, '2 Collins St', '2021-05-12', '1399', '2021-05-11', '', 'Sky View', 'Melbourne', 0, 1, 0, 0, 3, 'https://a2.muscache.com/im/pictures/34792065/bae84a3f_original.jpg?aki_policy=x_medium', '2021-05-13', '2021-05-15'),
+(3, 'Villa', '180° View, private pool villa', 10, 5, 3, 'Villa land, Launnie', '2021-05-13', '500', '', NULL, '180° View, private pool villa', 'Launceston', 1, 1, 1, 1, 4, 'https://a2.muscache.com/im/pictures/1faf9a4c-f839-44da-bd37-65ddc928379e.jpg?aki_policy=x_medium', '2021-05-20', '2021-05-25'),
+(4, 'Apartments', 'Newmarket - Young & Fennelly', 5, 3, 2, '30 Young Street,\r\n\r\nRandwick, NSW 2031', '2021-05-17', '530', '2021-05-17', NULL, 'Newmarket - Young & Fennelly', 'Randwick', 1, 1, 1, 1, 4, 'https://strap.domain.com.au/dream-homes-nsw/DreamHomes4903.jpg', '2021-05-17', '2021-05-20'),
+(5, 'House', 'Orme House', 8, 4, 3, '25 Orme Road, Buderim QLD 4556', '2021-05-10', '700', '2021-05-10', NULL, 'Orme House', 'Buderim', 1, 1, 1, 1, 4, 'https://rimh2.domainstatic.com.au/UVR5_h2WLqvaQW6iyO72uBvQrJ4=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016987480_1_1_210506_051200-w5033-h3353', '2021-05-25', '2021-05-27');
 
 -- --------------------------------------------------------
 
@@ -72,6 +75,14 @@ CREATE TABLE `bookings` (
   `rejected_reason` varchar(232) DEFAULT NULL,
   `checkout_date` varchar(232) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `user_id`, `price`, `start_date`, `end_date`, `created_at`, `no_guests`, `is_accepted`, `rejected_reason`, `checkout_date`) VALUES
+(2, 1, '500', '2021-05-14', '2021-05-14', '2021-05-14', 1, 0, '', ''),
+(3, 1, '150', '2021-05-14', '2021-05-17', '2021-05-14', 1, 0, '', '');
 
 -- --------------------------------------------------------
 
@@ -171,13 +182,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accommodations`
 --
 ALTER TABLE `accommodations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `messages`
